1379773ab3f967fa752236367afbcacaad58cc58
yashysinghal2463 <yashysinghal2463@gmail.com>
2025-03-04 23:24:19 +0530

    Time: 0 ms (100%), Space: 21.9 MB (39.06%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<=n;i++){
+            ans=ans^i;
+        }
+        for(auto j:nums){
+            ans=ans^j;
+        }
+        return ans;
+        
+    }
+};
